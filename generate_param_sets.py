5f038c313df076f3b3ee650b88595dc6a119e77e
Matthew Russell <matthewjohnrussell@gmail.com>
2026-03-16 19:54:46 +0000

    generate param sets

---
+++ generate_param_sets.py
@@ -0,0 +1,31 @@
+import polars as pl
+from itertools import product
+
+def expand_params(base_df: pl.DataFrame, varying: dict) -> pl.DataFrame:
+    vary_df = pl.DataFrame(
+        list(product(*varying.values())),
+        schema=list(varying.keys()),
+        orient="row"
+    )
+    return base_df.join(vary_df, how="cross")
+
+
+def main():
+    # hardcode for now. maybe parse args instead - how to pass lists of params cleanly?
+    varying = {
+        'chi_values': [0.4, 4.0, 40],
+        'cell_dist': ['uniform', 'random'],
+        'CCL21_added': ['true', 'false']
+    }
+
+    fixed_params = pl.read_csv("fixed_params.csv")
+    print(fixed_params)
+
+    all_param_sets = expand_params(fixed_params, varying)
+    print(all_param_sets)
+
+    all_param_sets.write_csv("all_params.csv")
+
+
+if __name__ == "__main__":
+    main()
