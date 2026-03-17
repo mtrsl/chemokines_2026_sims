4e7361d3cc734d3de9243b963ab1d01781f289fa
Matthew Russell <matthewjohnrussell@gmail.com>
2026-03-17 01:02:29 +0000

    args

--- generate_param_sets.py
+++ generate_param_sets.py
@@ -1,5 +1,6 @@
 import polars as pl
 from itertools import product
+import argparse
 
 
 def expand_params(base_df: pl.DataFrame, varying: dict) -> pl.DataFrame:
@@ -10,6 +11,16 @@ def expand_params(base_df: pl.DataFrame, varying: dict) -> pl.DataFrame:
 
 
 def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--fixed_params_file", type=str, required=True)
+    parser.add_argument("--all_params_file", type=str, required=True)
+
+    args = parser.parse_args()
+
+    fixed_params_file = args.fixed_params_file
+    all_params_file = args.all_params_file
+
     # hardcode for now. maybe parse args instead - how to pass lists of params cleanly?
     varying = {
         "chi_values": [0.4, 4.0, 40],
@@ -17,13 +28,13 @@ def main():
         "CCL21_added": ["true", "false"],
     }
 
-    fixed_params = pl.read_csv("fixed_params.csv")
+    fixed_params = pl.read_csv(fixed_params_file)
     print(fixed_params)
 
     all_param_sets = expand_params(fixed_params, varying)
     print(all_param_sets)
 
-    all_param_sets.write_csv("all_params.csv")
+    all_param_sets.write_csv(all_params_file)
 
 
 if __name__ == "__main__":
